e8d05c2c6c37b3c355673be5c0d0182e53ebfe7d
William Dias <smdwilliam89@gmail.com>
2023-01-11 22:37:35 +0000

    Fundamentos

---
+++ 2_fundamentos/fundamentos/src/App.js
@@ -0,0 +1,16 @@
+// COMPONENTS
+import FirstComponent from './components/FirstComponent';
+
+// CSS
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
