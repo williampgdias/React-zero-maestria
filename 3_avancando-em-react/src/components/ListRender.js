84b9babb4bb10d34755cafc570a3f6d079b45b3d
William Dias <smdwilliam89@gmail.com>
2023-01-12 15:17:13 +0000

    Propriedade Key

--- 3_avancando-em-react/src/components/ListRender.js
+++ 3_avancando-em-react/src/components/ListRender.js
@@ -3,14 +3,41 @@ import { useState } from 'react';
 const ListRender = () => {
   const [list] = useState(['William', 'Bárbara', 'Laura', 'Sidnei', 'Zeiry']);
 
+  const [users] = useState([
+    {
+      id: 1,
+      name: 'William',
+      age: 33,
+    },
+    {
+      id: 123412,
+      name: 'Bárbara',
+      age: 33,
+    },
+    {
+      id: 2,
+      name: 'Laura',
+      age: 3,
+    },
+  ]);
+
   return (
     <div>
       <ul>
-        {list.map((nome) => (
-          <li>{nome}</li>
+        {list.map((nome, i) => (
+          <li key={i}>{nome}</li>
+        ))}
+      </ul>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name} - {user.age} anos</li>
         ))}
       </ul>
     </div>
+
+  
+    
   );
 };
 
