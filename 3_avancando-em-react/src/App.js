2e293f07d4632d98f6e3890de026cffe5cd38485
William Dias <smdwilliam89@gmail.com>
2023-01-12 12:05:19 +0000

    Criando a seção 3, excluindo arquivos que não precisamos e inserindo IMG da pasta Public

---
+++ 3_avancando-em-react/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Avançando em React</h1>
+
+      {/* Imagem em public */}
+      <div>
+        <img src='/img1.jpeg' alt='Paisagem' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
