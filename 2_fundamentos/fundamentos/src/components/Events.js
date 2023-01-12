602b8700965cee9c5982531b0383c31c67bd1b97
William Dias <smdwilliam89@gmail.com>
2023-01-12 10:37:34 +0000

    Funções nos eventos

--- 2_fundamentos/fundamentos/src/components/Events.js
+++ 2_fundamentos/fundamentos/src/components/Events.js
@@ -8,6 +8,24 @@ const Events = () => {
       <div>
         <button onClick={handleMyEvent}>Clique aqui.</button>
       </div>
+      <div>
+        <button
+          onClick={() => {
+            console.log('Cliquei');
+          }}
+        >
+          Clique aqui também
+        </button>
+        <button
+          onClick={() => {
+            if (true) {
+              console.log('Isso não deveria existir');
+            }
+          }}
+        >
+          Clica aqui, por favor.
+        </button>
+      </div>
     </div>
   );
 };
