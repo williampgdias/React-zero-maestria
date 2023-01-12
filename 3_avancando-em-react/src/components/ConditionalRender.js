375b7a59799e4eec2e0f3285f62531e1b2c09289
William Dias <smdwilliam89@gmail.com>
2023-01-12 15:57:47 +0000

    If/Else no JSX

--- 3_avancando-em-react/src/components/ConditionalRender.js
+++ 3_avancando-em-react/src/components/ConditionalRender.js
@@ -3,11 +3,26 @@ import { useState } from 'react';
 const ConditionalRender = () => {
   const [x] = useState(false);
 
-  return <div>
-    <h1>Isso será exibido?</h1>
-    {x && <p>Se X for true, sim.</p>}
-    {!x && <p>Agora X é falso.</p>}
-  </div>;
+  const [name, setName] = useState('Matheus');
+
+  return (
+    <div>
+      <h1>Isso será exibido?</h1>
+      {x && <p>Se X for true, sim.</p>}
+      {!x && <p>Agora X é falso.</p>}
+
+      {name === 'João' ? (
+        <div>
+          <p>O nome é João</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado</p>
+        </div>
+      )}
+      <button onClick={() => setName('João')}>Clica aqui!</button>
+    </div>
+  );
 };
 
 export default ConditionalRender;
