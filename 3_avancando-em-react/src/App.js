ffd3fd0edabc854aea8972a8f6978469b2a28981
William Dias <smdwilliam89@gmail.com>
2023-01-18 21:56:00 +0000

    Renderização de lista em componente

--- 3_avancando-em-react/src/App.js
+++ 3_avancando-em-react/src/App.js
@@ -16,6 +16,12 @@ function App() {
   const name = 'Bárbara';
   const [userName] = useState('Laura');
 
+  const cars = [
+    { id: 1, brand: 'Ferrari', color: 'Amarela', newCar: true, km: 0 },
+    { id: 2, brand: 'KIA', color: 'Branco', newCar: false, km: 3434 },
+    { id: 3, brand: 'Renault', color: 'Azul', newCar: false, km: 234 },
+  ];
+
   return (
     <div className='App'>
       <h1>Avançando em React</h1>
@@ -38,10 +44,12 @@ function App() {
       {/* props */}
       <ShowUser name={userName} />
       {/* destructuring */}
-      <CarDetails brand='VW' km={100000} color='azul' newCar={false} />
-      {/* reaproveitando */}
       <CarDetails brand='Ford' color='Vermelha' km={0} newCar={true} />
       <CarDetails brand='Fiat' color='Branco' km={4500} newCar={false} />
+      {/* LOOP EM ARRAY DE OBJETOS */}
+      {cars.map((car) => (
+        <CarDetails brand={car.brand} color={car.color} km={car.km} newCar={car.newCar} />
+      ))}
     </div>
   );
 }
--- 3_avancando-em-react/src/components/CarDetails.js
+++ 3_avancando-em-react/src/components/CarDetails.js
@@ -1,13 +1,21 @@
 const CarDetails = ({ brand, km, color, newCar }) => {
   return (
     <div>
-      <h2>Detalhes do carro</h2>
-      <ul>
-        <li>Marca: {brand}</li>
-        <li>KM: {km}</li>
-        <li>Cor: {color}</li>
-      </ul>
-      {newCar && <p>Este carro é novo</p>}
+      <div>
+        <h2>Detalhes do carro</h2>
+        <ul>
+          <li>
+            Marca: <span>{brand}</span>
+          </li>
+          <li>
+            KM: <span>{km}</span>
+          </li>
+          <li>
+            Cor: <span>{color}</span>
+          </li>
+        </ul>
+        {newCar && <p>Este carro é novo</p>}
+      </div>
     </div>
   );
 };
