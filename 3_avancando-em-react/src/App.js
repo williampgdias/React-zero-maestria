c35917a59d5fe8e41efc7d9ea76b218561fc75ee
William Dias <smdwilliam89@gmail.com>
2023-01-18 11:45:41 +0000

    Introducao a props

--- 3_avancando-em-react/src/App.js
+++ 3_avancando-em-react/src/App.js
@@ -8,21 +8,33 @@ import Connemara from './assets/connemara.jpeg';
 import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
+import ShowUser from './components/ShowUser';
+import { useState } from 'react';
 
 function App() {
+  const name = 'Bárbara';
+  const [userName] = useState('Laura');
+
   return (
     <div className='App'>
       <h1>Avançando em React</h1>
 
       {/* Imagem em public */}
-      <div><img src='/img1.jpeg' alt='Paisagem' /></div>
+      <div>
+        <img src='/img1.jpeg' alt='Paisagem' />
+      </div>
 
       {/* Imagem em Assets */}
-      <div><img src={Connemara} alt='Cidade de Connemara vista de longe' /></div>
+      <div>
+        <img src={Connemara} alt='Cidade de Connemara vista de longe' />
+      </div>
 
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      <ShowUser name={name} />
+      <ShowUser name='William' />
+      <ShowUser name={userName} />
     </div>
   );
 }
