12c8630bb21039db431fa41cfd7cef965a63822b
William Dias <smdwilliam89@gmail.com>
2023-01-19 00:50:42 +0000

    CSS inline dinâmico

--- 4_css_react/src/App.js
+++ 4_css_react/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
 
+import { useState } from 'react';
+
 function App() {
+  const n = 15;
+
+  const [name] = useState('Bárbara');
+
   return (
     <div className='App'>
       {/* CSS GLOBAL */}
@@ -13,6 +19,12 @@ function App() {
 
       {/* INLINE CSS */}
       <p style={{ padding: '25px', fontSize: '2rem', color: 'blue' }}>Este elemento foi estilizado de forma inline</p>
+
+      {/* CSS INLINE DINÂMICO */}
+      <h2 style={n > 10 ? { color: 'purple' } : { color: 'pink' }}>CSS Dinâmico</h2>
+      <h2 style={n < 10 ? { color: 'purple' } : { color: 'pink' }}>CSS Dinâmico</h2>
+
+      <h2 style={name === 'William' ? { color: 'green', backgroundColor: '#000', padding: '25px' } : null}>CSS Dinâmico</h2>
     </div>
   );
 }
