2cb5e3176ca1ec91f2e794763b82edd288f2f4e7
William Dias <smdwilliam89@gmail.com>
2023-01-19 00:23:55 +0000

    CSS de componentes

--- 4_css_react/src/App.js
+++ 4_css_react/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className='App'>
+      {/* CSS GLOBAL */}
       <h1>React com CSS</h1>
+
+      {/* CSS DE COMPONENTE */}
+      <MyComponent />
+      <p>Este parágrafo é do APP.js</p>
     </div>
   );
 }
