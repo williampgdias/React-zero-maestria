587053cad1b2382f2aa475028ee590035f14b7fb
William Dias <smdwilliam89@gmail.com>
2023-01-20 21:58:45 +0000

    Label envolvendo input

--- 5_formularios-com-react/src/components/MyForm.js
+++ 5_formularios-com-react/src/components/MyForm.js
@@ -2,13 +2,18 @@ import './MyForm.css';
 const MyForm = () => {
   return (
     <div>
-      {/* 1 - criacao de form */}
+      {/* 1 - CRIAÇÃO DE FORM */}
       <form>
         <div>
           <label htmlFor='name'>Name:</label>
           <input type='text' name='name' placeholder='Write your name' />
         </div>
-        <input type="submit" value='Submit' />
+        {/* 2 - LABEL ENVOLVENDO INPUT */}
+        <label>
+          <span>Email:</span>
+          <input type='email' name='email' placeholder='Write your e-mail' />
+        </label>
+        <input type='submit' value='Submit' />
       </form>
     </div>
   );
