cb496652885dc96271668cc8e69780af1361701e
William Dias <smdwilliam89@gmail.com>
2023-03-07 13:56:02 +0000

    Adicionando dados com React

--- 7_requisicoes-http-com-react/httpreact/src/App.js
+++ 7_requisicoes-http-com-react/httpreact/src/App.js
@@ -6,6 +6,8 @@ const url = 'http://localhost:3000/products';
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
 
   // 1 - RESGATANDO DADOS
   useEffect(() => {
@@ -17,16 +19,56 @@ function App() {
     fetchData();
   }, []);
 
-  console.log(products);
+  // 2 - ADIÇÃO DE PRODUTOS
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product),
+    });
+  };
 
   return (
-    <div className='App'>
-      <h1>Products List</h1>
+    <div className='container'>
+      <h1 className='title'>Products List</h1>
+      <div className='add-products'>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input
+              type='text'
+              value={name}
+              name='name'
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Price:
+            <input
+              type='number'
+              value={price}
+              name='price'
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input className='button' type='submit' value='create' />
+        </form>
+      </div>
 
       <ul>
         {products.map((product) => (
-          <li key={product.id}>
-            {product.name} - €{product.price}
+          <li className='box-list' key={product.id}>
+            {product.name}
+            <br />€{product.price}
           </li>
         ))}
       </ul>
